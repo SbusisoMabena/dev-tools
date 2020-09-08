d75a8bbe2de56d77ced13018a8c0bc29d0d26a3d
Sbusiso J Mabena <sbusisomabena0@gmail.com>
2020-09-08 17:16:18 +0200

    update objectGenerator

--- ObjectGenerator.php
+++ ObjectGenerator.php
@@ -1,64 +1,76 @@
-public function getTables() {
-    
-    $query= "SHOW TABLES";
-    
-    $stmt = $this->conn->prepare($query);
-    $stmt->execute();
-
-    $res = $stmt->fetchAll();
-
-    $table_names = [];
-
-    $x = "";
-
-    $count = 0;
-
-    foreach($res as $table) {
-
-        $count ++;
-        $x = "";
-
-        $name_of_table = $table[0];
-        $count = count($table_names);
-        $table_names[$count]["name"] = $name_of_table;
-
-        $x = $x . "<?php \n\nclass $name_of_table"."Object{\n\n";
-
-        $getters = "";
-        $setters = "";
-
-        $query_tables = 'select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME= :tableName ';
-        $result  = $this->SELECT($query_tables,[":tableName" => $name_of_table],true,false);
-        $count_columns = 0;
-        foreach ($result as $key => $column_name) {
+<?php
 
 
-
-            $count_columns  = count($table_names[$count]["columns"]);
-            $table_names[$count]["columns"][$count_columns] = $column_name["COLUMN_NAME"];
-
-            $col_name = $column_name["COLUMN_NAME"];
-
-            $x = $x . "\npublic string $".$col_name.'="";';
-
-            $getters = $getters . "\n\npublic function get$col_name(): string {\n";
-            $getters = $getters . "\n\treturn $"."this->$col_name;\n}\n";
-
-            $setters = $setters . "\n\npublic function set$col_name(string $"."var_$col_name): void \n{\n";
-            $setters = $setters . "\n\t$"."this->$col_name =$"."var_$col_name ;\n}\n";
-
-        }
-
-        $x = "$x $getters $setters \n}\n\n?>";
-
-        //if ($count < 3) {
-            file_put_contents(TEMP.$name_of_table."Object.php",$x);
-        //}
-
-
+function connectToDatabase(array $config)
+{
+    $servername = $config['hostname'];
+    $username = $config['dbuser'];
+    $password = $config['dbpassword'];
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
     }
-
-    //file_put_contents(TEMP."tables",$x);
-    //file_put_contents(TEMP."tables",$x);
-
 }
+
+ function getTables()
+ {
+     $config = require "./config.php";
+     $conn = connectToDatabase($config);
+     $query = "SHOW TABLES";
+    
+     $stmt = $conn->prepare($query);
+     $stmt->execute();
+
+     $res = $stmt->fetchAll();
+
+     $table_names = [];
+
+     $x = "";
+
+     $count = 0;
+
+     foreach ($res as $table) {
+         $count ++;
+         $x = "";
+
+         $name_of_table = $table[0];
+         $count = count($table_names);
+         $table_names[$count]["name"] = $name_of_table;
+
+         $x = $x . "<?php \n\nclass $name_of_table"."Object{\n\n";
+
+         $getters = "";
+         $setters = "";
+
+         $query_tables = 'select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME= :tableName ';
+         $result  = $this->SELECT($query_tables, [":tableName" => $name_of_table], true, false);
+         $count_columns = 0;
+         foreach ($result as $key => $column_name) {
+             $count_columns  = count($table_names[$count]["columns"]);
+             $table_names[$count]["columns"][$count_columns] = $column_name["COLUMN_NAME"];
+
+             $col_name = $column_name["COLUMN_NAME"];
+
+             $x = $x . "\npublic string $".$col_name.'="";';
+
+             $getters = $getters . "\n\npublic function get$col_name(): string {\n";
+             $getters = $getters . "\n\treturn $"."this->$col_name;\n}\n";
+
+             $setters = $setters . "\n\npublic function set$col_name(string $"."var_$col_name): void \n{\n";
+             $setters = $setters . "\n\t$"."this->$col_name =$"."var_$col_name ;\n}\n";
+         }
+
+         $x = "$x $getters $setters \n}\n\n?>";
+
+         //if ($count < 3) {
+         file_put_contents(TEMP.$name_of_table."Object.php", $x);
+         //}
+     }
+
+     //file_put_contents(TEMP."tables",$x);
+    //file_put_contents(TEMP."tables",$x);
+ }
---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config = [];
+$config['hostname'] = "localhost";
+$config['dbuser'] = "";
+$config['dbpassword'] = "";
+$config['dbname'] = "";
+
+return $config;
